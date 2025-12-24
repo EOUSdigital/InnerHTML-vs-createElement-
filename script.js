68e8fee9687bfe996b72de1c25da131f853df92b
EOUSdigital <117541034+EOUSdigital@users.noreply.github.com>
2025-12-24 23:16:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+//TODO  🟦 Module 7 - DOM Manipulation: Lesson 09. InnerHTML vs createElement()
+
+//  Up to now you have:
+//  • Selected elements (querySelector, querySelectorAll)
+//  • Traversed (parentElement, children, nextElementSibling, childNodes, etc.)
+//  • Created elements and inserted them (createElement, append, prepend)
+
+//* In this lesson you compare two different ways of building/updating DOM:
+//  1. innerHTML – string-based updates
+//  2. createElement + DOM methods – node-based updates
+
+//  You will:
+//  • See what innerHTML really does to existing DOM.
+//  • Understand when it is convenient vs when it is dangerous.
+//  • Contrast it with a structured createElement approach.
+
+//TODO  1. Conceptual Overview
+
+//? 1.1 innerHTML
+
+
+
+
+
+
+
+
+
+
+
+
+
